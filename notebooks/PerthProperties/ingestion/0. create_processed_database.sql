d40452027b338d548b1e5e48971772beecb2c968
Helen Amin <helenamin8@gmail.com>
2021-10-21 18:43:58 +0800

    comment added

--- notebooks/PerthProperties/ingestion/0. create_processed_database.sql
+++ notebooks/PerthProperties/ingestion/0. create_processed_database.sql
@@ -1,4 +1,9 @@
 -- Databricks notebook source
+-- MAGIC %md
+-- MAGIC #### Create pp1_processed database to keep silver tables
+
+-- COMMAND ----------
+
 Create Database If not Exists pp1_processed
 Location "/mnt/perthpropdl/processed"
 
