468b2b4460089bd0b98ad62151fd0c168a3d0fb6
Helen Amin <helenamin8@gmail.com>
2021-10-21 11:55:59 +0800

    create-processed-database

---
+++ notebooks/PerthProperties/ingestion/0. create_processed_database.sql
@@ -0,0 +1,10 @@
+-- Databricks notebook source
+Create Database If not Exists pp1_processed
+Location "/mnt/perthpropdl/processed"
+
+-- COMMAND ----------
+
+Desc Database pp1_processed;
+
+-- COMMAND ----------
+
