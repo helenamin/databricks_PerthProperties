5ea43d7fb3952c0e96a8f9cc3dfb59c54f44c4fc
Helen Amin <helenamin8@gmail.com>
2021-10-21 18:37:00 +0800

    data loaded from perth_bronze table

--- notebooks/PerthProperties/ingestion/1. ingest_files.py
+++ notebooks/PerthProperties/ingestion/1. ingest_files.py
@@ -1,47 +1,18 @@
 # Databricks notebook source
-dbutils.widgets.text("p_file_date", "2021-08-01")
-v_file_date = dbutils.widgets.get("p_file_date")
-
-# COMMAND ----------
-
 # MAGIC %run "../includes/configuration"
 
 # COMMAND ----------
 
 # MAGIC %md
-# MAGIC ##### Step 1. Read the csv file using spark dataframe reader
+# MAGIC ##### Step 1. Load Perth_bronze table 
 
 # COMMAND ----------
 
-from pyspark.sql.types import StructType, StructField, IntegerType, StringType, DoubleType
-
-property_schema = StructType(fields = [StructField("address", StringType(), True),
-                                       StructField("suburb", StringType(), True),
-                                       StructField("state", StringType(), True),
-                                       StructField("postcode", IntegerType(), True),                                       
-                                       StructField("full_address", StringType(), True),
-                                       StructField("price", StringType(), True),                                        
-                                       StructField("sold_date", StringType(), True),
-                                       StructField("type", StringType(), True),
-                                       StructField("bedrooms", IntegerType(), True),
-                                       StructField("bathrooms", IntegerType(), True),
-                                       StructField("car_space", IntegerType(), True),
-                                       StructField("land_size", IntegerType(), True),
-                                       StructField("building_size", IntegerType(), True),
-                                       StructField("built_date", StringType(), True),
-                                       StructField("rent", StringType(), True),
-                                       StructField("rent_date", StringType(), True),                                       
-                                       StructField("agent", StringType(), True),
-                                       StructField("lat", DoubleType(), True),
-                                       StructField("lng", DoubleType(), True)                                       
-])
+df = spark.sql("Select * from pp1_raw.perth_bronze")
 
 # COMMAND ----------
 
-df = spark.read \
-    .option("header", True) \
-    .schema(property_schema) \
-    .csv(f"{raw_folder_path}/{v_file_date}/")
+display(df)
 
 # COMMAND ----------
 
@@ -112,7 +83,7 @@ CityOfPerth_df.dtypes
 # COMMAND ----------
 
 # MAGIC %md
-# MAGIC ##### Step 7. Write data to datalake
+# MAGIC ##### Step 7. Write data to datalake (Create perth_silver table in pp1_processed database)
 
 # COMMAND ----------
 
@@ -124,8 +95,8 @@ dbutils.notebook.exit("Success")
 
 # COMMAND ----------
 
-perth_silver%sql 
-Select * from pp1_processed.perth_silver
+# MAGIC %sql 
+# MAGIC Select * from pp1_processed.perth_silver
 
 # COMMAND ----------
 
