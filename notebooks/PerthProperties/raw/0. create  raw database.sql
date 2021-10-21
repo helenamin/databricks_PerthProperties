89469f79c416b17afcaae8e36e9cc602c02300f7
Helen Amin <helenamin8@gmail.com>
2021-10-21 17:58:18 +0800

    pp1_raw database created

---
+++ notebooks/PerthProperties/raw/0. create  raw database.sql
@@ -0,0 +1,14 @@
+-- Databricks notebook source
+-- MAGIC %md
+-- MAGIC #### Create pp1_raw database to keep bronze tables
+
+-- COMMAND ----------
+
+Create Database if Not Exists pp1_raw;
+
+-- COMMAND ----------
+
+Desc Database pp1_processed;
+
+-- COMMAND ----------
+
