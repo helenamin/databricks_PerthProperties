9331924d72bed290a2c66a6c4acb91bb4b1d2a46
Helen Amin <helenamin8@gmail.com>
2021-10-23 18:54:17 +0800

    Update: .gitignore README.md dataset factory linkedService notebooks pipeline trigger

---
+++ notebooks/PerthProperties/includes/configuration.py
@@ -0,0 +1,10 @@
+# Databricks notebook source
+raw_folder_path = "/mnt/perthpropdl/raw"
+bronze_folder_path = "/mnt/perthpropdl/bronze"
+silver_folder_path = "/mnt/perthpropdl/silver"
+gold_folder_path = "/mnt/perthpropdl/gold"
+# processed_folder_path = "/mnt/perthpropdl/processed"
+# presentation_folder_path = "/mnt/perthpropdl/presentation"
+
+# COMMAND ----------
+
